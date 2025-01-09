d31b2f1f1626137cf4fe6a9c5f9de987ecf15a52
Abhishek-Kole <koleabhishek20020@gmail.com>
2025-01-09 08:44:09 +0530

    added arithmetic.java

---
+++ TL_GIT/src/main/java/DevSecOps/TL_GIT/Arithmetic.java
@@ -0,0 +1,13 @@
+package DevSecOps.TL_GIT;
+
+public class Arithmetic 
+{
+	public static void main(String[] args)
+	{
+		int a= 5;
+		int b = 5;
+		
+		System.out.println("ans :" + (a+b));
+	
+	}
+}
